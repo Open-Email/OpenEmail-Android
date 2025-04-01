08bf0d781361c699e1a4ebe6e16c53367bf83a79
Anton Akimchenko <akimchenko.antony@icloud.com>
2025-04-01 18:12:59 +0400

    bumped versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.mercata.openemail"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
